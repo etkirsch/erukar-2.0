514d33e76091af8a6eda11e903ae24f41324e917
Evan Kirsch <etkirsch@gmail.com>
2019-12-15 17:51:50 -0500

    fix it up

--- ui/landing/content-area.js
+++ ui/landing/content-area.js
@@ -2,15 +2,17 @@ import React from 'react'
 import ContentRow from './content-row'
 
 export default function LandingContentArea ({ suggestions }) {
-  const length = Math.ceil(suggestions.length / 3)
-  const segmentedSuggestions = Array.from({ length }, (v, k) => 
-    suggestions.slice(k*3, (k+1)*3)
-  )
-  console.log(segmentedSuggestions)
+  function segmentSuggestions () {
+    const length = Math.ceil(suggestions.length / 3)
+    const segmentedSuggestions = Array.from({ length }, (_, k) => 
+      suggestions.slice(k*3, (k+1)*3)
+    )
+    return segmentedSuggestions
+  }
 
   return (
     <div className='content-area'>
-      {segmentedSuggestions.map((segment, i) => {
+      {segmentSuggestions().map((segment, i) => {
         return <ContentRow
           key={`landing-content-row-${i}`}
           suggestions={segment}
