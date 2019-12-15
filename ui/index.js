4bc22cab04808749afb729060d9dfa72654582e4
Evan Kirsch <etkirsch@gmail.com>
2019-12-15 17:25:51 -0500

    add font-awesome, fix hamburger

--- ui/index.js
+++ ui/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
+library.add(fas, faBars)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
