67e54dd2f5778df22d6725ee5a2b6185aa1236cb
Evan Kirsch <etkirsch@gmail.com>
2020-03-09 10:05:51 -0400

    add date

--- ui/episodes/viewer/header.jsx
+++ ui/episodes/viewer/header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
 export default function EpisodeViewerHeader ({ episode }) {
+  function formattedDate () {
+    let episodeDate = new Date(episode.date)
+    let datestring = episodeDate.toDateString()
+      .split(' ')
+      .slice(1)
+      .join(' ')
+
+    if (episodeDate.getTime() < Date.now()) {
+      return `Took place on ${datestring}.`
+    }
+    let timestring = episodeDate.toLocaleTimeString()
+      .split(' ')
+      .slice(0,2)
+      .join(' ')
+
+    return `Will take place on ${datestring} at ${timestring}.`
+  }
+
   return (
     <div className='header'>
       <div className='title'>{episode.title}</div>
       <div className='description'>{episode.description}</div>
+      <div className='date'>{formattedDate()}</div>
       <div className='gm'>Game Mastered by {episode.gm}</div>
     </div>
   )
